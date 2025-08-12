389660745ea29a0d32724524804af21311ee1a36
ebubechi-ihediwa <ebubechiihediwa@gmail.com>
2025-08-12 23:46:02 +0100

    Feat: Preparing branch for update

---
+++ src/app/dashboard/(doctor)/page.tsx
@@ -0,0 +1,11 @@
+const DoctorDashboard = () => {
+    return (
+        <>
+            <div className="w-full min-h-screen bg-white" >
+
+            </div>
+        </>
+    );
+}
+
+export default DoctorDashboard;
